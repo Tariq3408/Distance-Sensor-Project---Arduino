d3fb76237f0500a5d1d8958d89453e911e622c0b
Tariq Shaikh <86179877+Tariq3408@users.noreply.github.com>
2024-06-12 15:14:50 -0400

    Add files via upload

--- DIstance_sensor.ino
+++ DIstance_sensor.ino
@@ -1,28 +1,67 @@
-const int echo = 13;
+const int ledRED = 8;
+const int ledYELLOW = 9;
+const int ledGREEN = 10;
+
 const int trig = 12;
+const int echo = 13;
 
-int distance = 0;
-int duration = 0;
+long duration;
+int distance;
 
-int led = 3;
-
-void setup()
-{
-  pinMode(trig, OUTPUT);
+void setup() {
   pinMode(echo, INPUT);
+  pinMode(trig, OUTPUT);
+
+  pinMode(ledRED, OUTPUT);
+  pinMode(ledYELLOW, OUTPUT);
+  pinMode(ledGREEN, OUTPUT);
+
   Serial.begin(9600);
-
-  pinMode(led, OUTPUT);
 }
+void loop() {
+digitalWrite(trig, LOW);
+delay(2);
 
-void loop()
+digitalWrite(trig, HIGH);
+delay(10);
+digitalWrite(trig, LOW);
+
+duration = pulseIn(echo, HIGH);
+distance = duration * 0.034/2;
+
+Serial.println(distance);
+
+if(distance <= 20)
 {
-  digitalWrite(trig, HIGH);
-  delay(1000);
-  digitalWrite(trig, LOW);
-  duration = pulseIn(echo, HIGH);
-  distance = (duration/2)/28.5;
-  Serial.println(distance);
-
-  digitalWrite(led, 1);
+  digitalWrite(ledRED, HIGH);
+  digitalWrite(ledYELLOW, LOW);
+  digitalWrite(ledGREEN, LOW);
+}
+if(distance >= 21 && distance <= 40)
+{
+  digitalWrite(ledRED, LOW);
+  digitalWrite(ledYELLOW, HIGH);
+  digitalWrite(ledGREEN, LOW)  ;
+}
+if(distance >= 41 && distance <= 60)
+{
+  digitalWrite(ledRED, LOW);
+  digitalWrite(ledYELLOW, LOW);
+  digitalWrite(ledGREEN, HIGH);
+}
+if(distance>= 61)
+{
+  digitalWrite(ledRED, HIGH);
+  delay(100);
+  digitalWrite(ledRED, LOW);
+  delay(100);
+    digitalWrite(ledYELLOW, HIGH);
+  delay(100);
+  digitalWrite(ledYELLOW, LOW);
+  delay(100);
+    digitalWrite(ledGREEN, HIGH);
+  delay(100);
+  digitalWrite(ledGREEN, LOW);
+  delay(100);
+}
 }
